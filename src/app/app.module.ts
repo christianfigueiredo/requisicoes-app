47b3b63fef6e34a398d2e657979ad90bcfb11cb2
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-11-25 16:36:35 -0300

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/enviroments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/enviroments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCtznKNj0OLGPHNvj58LRIq_3pVnp62Gqw",
+        authDomain: "req-app-218dc.firebaseapp.com",
+        databaseURL: "https://req-app.firebaseio.com",
+        projectId: "req-app-218dc",
+        storageBucket: "req-app-218dc.appspot.com",
+        messagingSenderId: "646730939552",
+        appId: "1:646730939552:web:d8acaa5dde9fe5f6785dc5",
+        measurementId: "G-Q2R40K30VR"
+    }
+  };
+
